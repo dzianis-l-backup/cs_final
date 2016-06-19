2d45f7008dde3e43855ecbefe553ec79fb23fea9
DzianisL <gestaltldi@yandex.ru>
2016-06-19 12:47:05 +0300

    cs

---
+++ csSniffer/csSniffer/Window.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace csSniffer
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 17);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(673, 451);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(700, 198);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(51, 19);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "UDP";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox3.Location = new System.Drawing.Point(700, 230);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(51, 22);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "TCP";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(699, 138);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(254, 26);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(695, 109);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Device / Peripheral";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(699, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 72);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "clear session";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkGray;
+            this.button3.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(773, 198);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 62);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "save current traffic ";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk_1);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button4.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(794, 422);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 46);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "close";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(828, 109);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(125, 22);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = " Start capturing";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(986, 481);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Window";
+            this.Text = "Sniffer (Trafic Analyzer)";
+            this.Load += new System.EventHandler(this.Window_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+
+    }
+}
+
---
+++ csSniffer/csSniffer/Window.cs
@@ -0,0 +1,302 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+
+
+namespace csSniffer
+{
+   
+    public enum Protocol//id of a protocol in a packet packet
+    {
+        ICMP = 1,
+        TCP = 6,
+        UDP = 17,        
+        Unknown = -1
+    }
+    
+    public partial class Window : Form
+    {
+        public static void CatchFunction(Exception ex)
+        {
+            StreamWriter Strw;
+            FileInfo Errors = new FileInfo(Directory.GetCurrentDirectory() + @"\Errors.txt");
+            if (!Errors.Exists)
+                Strw = new StreamWriter(Errors.Create(), System.Text.Encoding.UTF8);
+            else
+                Strw = Errors.AppendText();
+            Strw.WriteLine("\r\n*Errors logging at: {0}*", DateTime.Now);
+            Strw.WriteLine("Message: " + ex.Message + "; source: " + ex.Source + "; data: " + ex.Data + "; stacktrace: "+ex.StackTrace);
+            Strw.Flush();
+            MessageBox.Show("An error has occured: " + ex.Message + ". For more information see the Errors.txt logs.");
+            
+        }
+        StreamWriter SmWr;
+        private Socket socket;//Socket that will be sniffered
+        private byte[] buffer;//Buffer to store all the information
+        public Window()
+        {
+            try
+            {
+                InitializeComponent();
+                this.AutoSize = true;
+                this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+                this.ControlBox = false;
+                FileInfo Logs = new FileInfo(Directory.GetCurrentDirectory() + @"\Logs.txt");
+                if (Logs.Exists)
+                    Logs.Delete();
+                SmWr = new StreamWriter(Logs.Create(), Encoding.UTF8);
+                string HostName = Dns.GetHostName();
+                IPHostEntry HosyEntry = Dns.GetHostEntry(HostName);// Dns servise 
+                if (HosyEntry.AddressList.Length > 0)
+                {
+                    foreach (IPAddress ip in HosyEntry.AddressList)
+                    {
+                        if( ip.AddressFamily == AddressFamily.InterNetwork)
+                            comboBox1.Items.Add(ip.ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                CatchFunction(ex);
+            }
+
+        }
+        private void OnReceive(IAsyncResult ar)
+        {
+            try
+            {
+                int nReceived = socket.EndReceive(ar);
+                Print(buffer, nReceived);
+                buffer = new byte[4096];
+                socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None,
+                    OnReceive, null);
+            }
+            catch (Exception ex)
+            {
+                //CatchFunction(ex);
+            }
+
+        }
+        void Print(byte[] buf, int len)
+        {
+            try
+            {
+                csSniffer.DataStructures.IPHeader ipHeader = new csSniffer.DataStructures.IPHeader(buf, len);
+                bool UDP = checkBox3.Checked;
+                bool TCP = checkBox3.Checked;
+                if ((ipHeader.ProtocolType == Protocol.UDP && UDP == true) || (ipHeader.ProtocolType == Protocol.TCP && TCP == true) || (ipHeader.ProtocolType == Protocol.ICMP))
+                {
+                    string temp = string.Empty;
+                    for (int i = 0; i < len; i++)
+                    {
+                        temp += buf[i].ToString("X2") + "  ";
+                        if ((i + 1) % 16 == 0 && i != 0)
+                        {
+                            //string txt = Encoding.ASCII.GetString(buf, i, 16);
+                            temp += "\r\n";
+                        }
+                        else { }
+
+                    }
+                    richTextBox1.BeginInvoke(new Action(() => richTextBox1.AppendText("\r\n\r\n\r\n" + "[---------------------------Raw data---------------------------]\r\n" + temp + "\r\n" + Parse(buf, len) + " \r\n")));
+                    SmWr.WriteLine("\r\n\r\n\r\n" + "[---------------------------Raw data---------------------------]\r\n" + temp + "\r\n" + Parse(buf, len) + " \r\n");
+                    SmWr.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                //CatchFunction(ex);
+            }
+        }
+
+        public string Parse(byte[] buf, int len)
+        {
+            try
+            {
+                csSniffer.DataStructures.IPHeader ipHeader = new csSniffer.DataStructures.IPHeader(buf, len);
+                string IpHeader = "\r\n[---------------------------New packet---------------------------]\r\n" +
+                                    "--IP header: " +
+                                     "-Version: " + ipHeader.Version +
+                                     "\r\n-IP Header length: " + ipHeader.HeaderLength +
+                                     "\r\n-TOS: " + ipHeader.DifferentiatedServices +
+                                     "\r\n-Total length: " + ipHeader.TotalLength +
+                    "\r\n-Identification: " + ipHeader.Identification +
+                    "\r\n-Flags: " + ipHeader.Flags +
+                    "\r\n-Fragmentation: " + ipHeader.FragmentationOffset +
+                    "\r\n-TTL: " + ipHeader.TTL +
+                    "\r\n-Protocol: " + ipHeader.ProtocolType +
+                    "\r\n-Header checksum: " + ipHeader.Checksum +
+                    "\r\n-Source address: " + ipHeader.SourceAddress +
+                    "\r\n-Destination address: " + ipHeader.DestinationAddress;
+                    
+                if (ipHeader.ProtocolType == Protocol.TCP)
+                {
+                    csSniffer.DataStructures.TCPHeader tcpHeader = new csSniffer.DataStructures.TCPHeader(ipHeader.Data, len - Convert.ToInt32(ipHeader.HeaderLength));
+                    IpHeader += "\r\n\r\n--TCP header: " +
+                        "\r\n-Source port: " + tcpHeader.usSourcePort +
+                        "\r\n-Destination port: " + tcpHeader.usDestinationPort +
+                        "\r\n-Sequence number: " + tcpHeader.uiSequenceNumber +
+                        "\r\n-Acknowledge number: " + tcpHeader.uiAcknowledgeNumber +
+                        "\r\n-Offset & space & flag: " + tcpHeader.usOffsetSpaceFlag +
+                        "\r\n-Window: " + tcpHeader.usWindow +
+                        "\r\n-Checksum: " + tcpHeader.usChecksum.ToString("X2").ToLower() +
+                        "\r\n-Urgent pointer: " + tcpHeader.usUrgentPointer;
+                }
+                if (ipHeader.ProtocolType == Protocol.UDP)
+                {
+                    csSniffer.DataStructures.UDPHeader udpHeader = new csSniffer.DataStructures.UDPHeader(ipHeader.Data, len - Convert.ToInt32(ipHeader.HeaderLength));
+                    IpHeader += "\r\n\r\n--UDP header: " +
+                        "\r\n-Source port: " + udpHeader.usSourcePort +
+                        "\r\n-Destination port: " + udpHeader.usDestinationPort +
+                        "\r\n-Datagram length: " + udpHeader.usLength +
+                        "\r\n-Checksum: " + udpHeader.usCheckSum.ToString("X2").ToLower();                        
+                }
+                if (ipHeader.ProtocolType == Protocol.ICMP)
+                {
+                    csSniffer.DataStructures.ICMPHeader udpHeader = new csSniffer.DataStructures.ICMPHeader(ipHeader.Data, len - Convert.ToInt32(ipHeader.HeaderLength));
+                    IpHeader += "\r\n\r\n--ICMP header: " +
+                        "\r\n-Type: " + udpHeader.Type +
+                        "\r\n-Status: " + udpHeader.Serial +
+                        "\r\n-Checksum: " + udpHeader.usCheckSum.ToString("X2").ToLower();
+                }
+                return IpHeader;
+
+            }
+            catch (Exception ex)
+            {
+                //CatchFunction(ex);
+                return "";
+            }
+        }
+        
+
+       
+      
+
+        /// <summary>
+        /// /////////////
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+      
+
+      
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            // Get file name.
+            string name = saveFileDialog1.FileName;
+            // Write to the file name selected.
+            // ... You can write the text from a TextBox instead of a string literal.
+            File.WriteAllText(name, richTextBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Window_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CloseProg();
+        }
+
+        public void CloseProg()
+        {
+            MessageBox.Show("*Thank you for the sniffer usage* (Written by Dzianis Leanenka, BSUIR, 2016)");
+            SmWr.Close();
+            SmWr.Dispose();
+            this.Close();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((sender as CheckBox).Checked)
+                {
+                    try
+                    {
+                        (sender as CheckBox).Text = "Stop Capturing";
+                        socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);// creation of a new socket
+                        socket.Bind(new IPEndPoint(IPAddress.Parse(comboBox1.SelectedItem.ToString()), 0)); // bind the socket with the chosen IP-address
+                        socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        CatchFunction(ex);
+                    }
+                    byte[] byInc = new byte[] { 1, 0, 0, 0 };
+                    byte[] byOut = new byte[4];
+                    buffer = new byte[4096];//buffer for receiving data
+                    socket.IOControl(IOControlCode.ReceiveAll, byInc, byOut);// promiscuous mode
+                    socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, OnReceive, null);// start receiving packets in asynchronous mode; buffer,size,socketFlags,callback,state
+
+                }
+                else
+                {
+                    socket.Close();
+                    (sender as CheckBox).Text = "Start capturing";
+                }
+            }
+            catch (Exception ex)
+            {
+                //CatchFunction(ex);
+            }
+        }
+
+        private void saveFileDialog1_FileOk_1(object sender, CancelEventArgs e)
+        {
+            // Get file name.
+            string name = saveFileDialog1.FileName;
+            // Write to the file name selected.
+            // ... You can write the text from a TextBox instead of a string literal.
+            File.WriteAllText(name, richTextBox1.Text);
+        }
+    }   
+        
+}
+
+
